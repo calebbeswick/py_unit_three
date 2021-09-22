5e4d35164f1e97f675b04cc8d893b41a7e9220ee
calebbeswick <beswica@ssfs.org>
2021-09-22 15:15:40 -0400

    Worked on egg.py

--- egg.py
+++ egg.py
@@ -1,9 +1,30 @@
 # Start writing your functions below this line
 
 
+def top_of_egg():
+    print("  _______")
+    print(" /       \\")
+    print("/         \\")
+def middle_of_egg():
+    print("-\"-\'-\"-\'-\"-")
+def bottom_of_egg():
+    print("\\         /")
+    print(" \\_______/")
 def main():
+    top_of_egg()
+    bottom_of_egg()
+    middle_of_egg()
+    top_of_egg()
+    bottom_of_egg()
+    middle_of_egg()
+    bottom_of_egg()
+    top_of_egg()
+    middle_of_egg()
+    bottom_of_egg()
     # all of your function calls should go here. Remove the word "pass" before adding function calls.
-    pass
+
 
 if __name__ == '__main__':
     main()
+
+
